78232040068e1aa38ddf673077494bc51ba11adf
ram <baburam_nabik@bpointer.in>
2025-03-10 19:19:00 +0530

    added get score api

--- lib/JuicyScoreRoute.dart
+++ lib/JuicyScoreRoute.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:juicyscore_flutter/initialization_sdk.dart';
@@ -20,6 +21,7 @@ class _JuicyScoreState extends State<JuicyScoreRoute> {
   int panStartTime = 0;
   Size? screenSize;
   bool initiated = false;
+  String sessionId = "";
 
   @override
   void initState() {
@@ -53,8 +55,10 @@ class _JuicyScoreState extends State<JuicyScoreRoute> {
               }
             case "session":
               {
+                sessionId = call.arguments;
                 initiated = true;
                 _log(call.arguments);
+                _log("SESSION_ID: ${call.arguments}");
                 break;
               }
             case "error":
@@ -130,6 +134,66 @@ class _JuicyScoreState extends State<JuicyScoreRoute> {
     debugPrint(arguments);
   }
 
+  getJuicyScoreApi(BuildContext context) async {
+    // Initialize Dio instance
+    Dio dio = Dio();
+
+    // Set your session and endpoint here
+    String session = sessionId;
+    String token =
+        "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2xhdW5jaHBhZC5zd2lwZWxvYW4uaW4vYXBpL3YxL2F1dGgvbG9naW4iLCJpYXQiOjE3NDE2MDEwNjksImV4cCI6MTc0NDE5MzA2OSwibmJmIjoxNzQxNjAxMDY5LCJqdGkiOiJMVkNER1pWMlNJN3RvdU00Iiwic3ViIjoiNjk2MzIiLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.D2G0w_9pR7ah5USmY_pS-3OaSGwmDO9gR-2PTP8zW8I";
+    String apiEndpoint = 'https://launchpad.swipeloan.in/api/v1/getJuicyScore';
+
+    // Prepare headers
+    Map<String, String> headers = {
+      // 'token': token,
+    };
+
+    try {
+      // Send POST request
+      Response response = await dio.post(
+        apiEndpoint,
+        data: {"session_id": session, 'token': token},
+        options: Options(
+          headers: headers,
+        ),
+      );
+
+      // Handle the response
+      if (response.statusCode == 200) {
+        print('Request was successful: ${response.data}');
+        _showDialog(context, 'Success', 'Request was successful: ${response.data["data"]["antifraud_score"]}');
+      } else {
+        _showDialog(context, 'failure', 'Request failed: ${response.statusCode}');
+
+        print('Request failed with status: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error: $e');
+      _showDialog(context, 'Error', 'Request was successful: ${e.toString()}');
+    }
+  }
+
+  void _showDialog(BuildContext context, String title, String message) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title),
+          content: Text(message),
+          actions: <Widget>[
+            TextButton(
+              child: Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     screenSize = MediaQuery.of(context).size;
@@ -143,7 +207,7 @@ class _JuicyScoreState extends State<JuicyScoreRoute> {
               onPointerDown: (event) {
                 _juicyscoreFlutterPlugin.setTouchRadius(event.radiusMajor);
               },
-              child: const Scaffold(
+              child: Scaffold(
                 extendBodyBehindAppBar: false,
                 body: SafeArea(
                   child: Column(
@@ -152,6 +216,13 @@ class _JuicyScoreState extends State<JuicyScoreRoute> {
                       ScrollText(),
                       TestTapButton(),
                       DismissButton(),
+                      SizedBox(height: 100),
+                      ElevatedButton(
+                        onPressed: () async {
+                          await getJuicyScoreApi(context);
+                        },
+                        child: Text('Call Get Score Api'),
+                      )
                     ],
                   ),
                 ),
